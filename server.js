ac0fc7d30fdec92cc7978f6b1102b8b9531e7bf4
Naveen Thurimerla <naveen_thurimerla@lilly.com>
2021-02-21 18:31:37 +0530

    add sqlite model

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+
+app = express()
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
